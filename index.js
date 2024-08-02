62e8e6dae475c354fed3cbb394ebf895d98c2b33
Frederik Shull <zlyfer@pm.me>
2024-08-02 17:17:38 +0200

    update

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ client.on("ready", () => {
   changeColor();
 
   console.log(`Bot ready.`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 /* ------- Function ------- */
@@ -48,7 +49,7 @@ function changeColor() {
               .setColor(hexColor)
               .then(() => {
                 fs.writeFileSync("./lastColor.json", `{ "lastColor": "${hexColor}" }`);
-                console.log(`[${currentTime()}] Changed color to: ${hexColor}.`);
+                console.log(`Changed color to: ${hexColor}.`);
               })
               .catch(console.warn);
             color = color.shiftHue(1 / 90);
@@ -90,19 +91,6 @@ function rateLimit(rateLimitInfo) {
 }
 function error(errorInfo) {
   console.table(errorInfo);
-}
-
-/* ---- Other Functions --- */
-
-function currentTime() {
-  let time = new Date(Date.now());
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-  let seconds = time.getSeconds();
-  if (hours < 10) hours = `0${hours}`;
-  if (minutes < 10) minutes = `0${minutes}`;
-  if (seconds < 10) seconds = `0${seconds}`;
-  return `${hours}:${minutes}:${seconds}`;
 }
 
 /* ---------- Bot Shutdown ---------- */
